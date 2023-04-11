f66fbc713a68b2aac5fcc3addb6942f2fb8e2a9a
Izhal-Brown <informatika749@gmail.com>
2023-04-11 14:26:54 +0800

    frontend2

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,13 +14,13 @@ const Contact = () => {
         <div className="row">
           <div className="col-12">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6986.771103663534!2d76.99275607711007!3d28.886888929272477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390da5e51463d4c9%3A0xe5a485e2ac7c3d4a!2sMandaura%2C%20Haryana%20131103!5e0!3m2!1sen!2sin!4v1669909087902!5m2!1sen!2sin"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3984.2668257175164!2d120.20868591433509!3d-3.0228486408370676!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2d915f1703c09e7f%3A0x1e93288ee8563d24!2sHonda%20Sanggar%20Laut%20Palopo!5e0!3m2!1sid!2sid!4v1681194375680!5m2!1sid!2sid"
               width="600"
               height="450"
-              className="border-0 w-100"
-              allowFullScreen=""
+              style="border:0;"
+              allowfullscreen=""
               loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
+              referrerpolicy="no-referrer-when-downgrade"
             ></iframe>
           </div>
           <div className="col-12 mt-5">
